cc72601ea332b263b8c99ceb02607599b5a1b037
googlermridul <goooglermridul@gmail.com>
2022-03-06 01:39:05 +0600

    started todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <BrowserRouter>
+            <Link to={"home"}>home</Link>
+            <Link to={"about"}>about</Link>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="home" element={<Home />} />
+               <Route path="about" element={<About />} />
+            </Routes>
+         </BrowserRouter>
+      </>
+   );
 }
 
 export default App;
---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+   return (
+      <div>
+         <h3>about page</h3>
+      </div>
+   );
+};
+
+export default About;
