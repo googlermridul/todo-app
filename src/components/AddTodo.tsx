b7384f47895d6e79ef2060c4eb195e0bd7486ad4
googlermridul <goooglermridul@gmail.com>
2022-03-18 01:15:03 +0600

    added flaticon

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-import left from "../images/left.png";
-import search from "../images/search2.png";
-import sun from "../images/sun.png";
+import left from "../images/left-arrow.png";
+import tag from "../images/tag.png";
+import bell from "../images/bell.png";
+import moon from "../images/crescent-moon.png";
+import pushpin from "../images/pushpin.png";
 
 const AddTodo = () => {
    return (
@@ -16,17 +18,26 @@ const AddTodo = () => {
                   </div>
                   <h2>Add ToDo</h2>
                   <div className="mode shadow-">
-                     <img src={sun} alt="" />
+                     <img src={moon} alt="" />
                   </div>
                </div>
                <div className="input-group">
-                  <img className="input-icon" src={search} alt="" />
-                  <input
-                     className="shadow-sm"
-                     type="text"
-                     placeholder="Search tasks..."
-                  />
+                  <label htmlFor="plan">What are your planning?</label>
+                  <input type="text" id="plan" />
                </div>
+               <div className="input-group2">
+                  <img className="input-icon" src={tag} alt="" />
+                  <input className="" type="text" placeholder="Category" />
+               </div>
+               <div className="input-group2">
+                  <img className="input-icon" src={bell} alt="" />
+                  <input className="" type="text" placeholder="May 29, 2022" />
+               </div>
+               <div className="input-group2">
+                  <img className="input-icon" src={pushpin} alt="" />
+                  <input className="" type="text" placeholder="Add note" />
+               </div>
+               <button className="submit-btn">Add Todo</button>
             </div>
          </div>
       </div>
